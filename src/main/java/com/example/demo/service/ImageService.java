f38613ec14f070e436a5b0344313e68f14393827
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-08-08 19:40:54 -0300

    feat: criado o metodo de salvar image no ImageServica

--- src/main/java/com/example/demo/service/ImageService.java
+++ src/main/java/com/example/demo/service/ImageService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.ImageResponseDTO;
+import com.example.demo.entity.Image;
 import com.example.demo.repository.ImageRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -19,7 +21,20 @@ public class ImageService {
         return imageRepository.findAll().stream().map(ImageResponseDTO :: new).toList();
     }
 
+    public ImageResponseDTO save(ImageResponseDTO dto){
+        Image img= imageRepository.save(data(dto));
+        return new ImageResponseDTO(img);
+    }
 
 
+    private Image data(ImageResponseDTO dto){
+        Image img = new Image();
+
+        img.setImageName(dto.imageName());
+        img.setCreateTime(dto.createTime());
+        img.setSizeBytes(dto.sizeBytes());
+
+        return img;
+    }
 
 }
